d9c84d0a67fb2dddf36ff4b7a348b64416ebcdb6
raghavk0077 <raghavkhanna1301@gmail.com>
2023-04-19 00:19:29 +0530

    Made all the functionality of crud

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ let express = require('express');
 const app = express();
 
 var path = require('path');
-app.use(express.urlencoded({extended:false}));
+// app.use(express.urlencoded({extended:false}));
 app.set('views', path.join(__dirname,'view'));
 app.set('view engine', 'ejs');
 
+
+
+
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({extended:true}));
+
+// 
 let db = require('./database');
 
 let msg = "";
@@ -22,16 +29,19 @@ app.get('/adduser', function(req,res){
 })
 
 app.get('/listuser', function(req,res){
+    let msg = "";
+    if(req.query['msg'] != ""){
+        msg = req.query['msg'];
+    }
     let sql__list = "select * from users;"
     db.query(sql__list, function(error, result, field){
         if(error){
             res.redirect('/');
         }
         else{
-            res.render('userlistview', {data:result});
+            res.render('userlistview', {data:result, msg:msg});
         }
     })
-    // res.render('userlistview');
 })
 
 app.post('/addusersubmit', function(req,res){
@@ -56,7 +66,48 @@ app.post('/addusersubmit', function(req,res){
             res.redirect('/?msg='+msg);
     });
   
-})  
+});
+
+// edit user
+app.get('/edituser', function(req,res){
+    let sql = "select* from users where id = "+req.query['id'];
+    db.query(sql, function(error, result, field){
+        if(error)
+            res.redirect('/');
+        else
+            res.render('edituser', {data:result});
+    })
+})
+
+app.post('/editusersubmit', function(req, res){
+    var id = req.body.id;
+    let name = req.body.name;
+    let email = req.body.email;
+    let address = req.body.address;
+    let mobile = req.body.mobile;
+
+    let sql__edit = "update users set name ='"+name+"', email ='"+email+"', address ='"+address+"', mobile = "+mobile+" where id ="+id;
+    db.query(sql__edit, function(error, result, field){
+        if(error){
+            res.redirect('/listuser?msg=can not update user');
+            console.log(error);
+        }
+        else{
+            res.redirect('/listuser?msg=user updated');
+        }
+    })
+})
+
+//delete user
+app.get('/deluser', function(req,res){
+    let sql__del = "Delete from users where id = "+req.query['id'];
+    db.query(sql__del, function(error, result, field){
+        if(error)
+            res.redirect('/listuser?msg=unable to delete user');
+        else
+            res.redirect('/listuser?msg=User Deleted');
+    })
+})
 
 app.listen(3000,function(){
     console.log("Server started");
